aee34aa0355b30a72dbb8ee562def7afa4e66207
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-06-15 21:05:33 +0200

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'simple-crm';
 
-  
 }
